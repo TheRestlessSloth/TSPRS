2f4c22ee3174c68164d81639d2404fa67baeb862
TheRestlessSloth <TheRestlessSloth@users.noreply.github.com>
2020-04-19 20:13:31 -0700

    new

--- IO/8segment indicator/8segment indicator/main.cpp
+++ IO/8segment indicator/8segment indicator/main.cpp
@@ -9,16 +9,11 @@
 #define F_CPU 16000000UL
 #include <util/delay.h>
 
-#define delay_time 10
+#define delay_time 1000
 
-#define A 0b01100000
+int seq[10] = {0b10100000, 0b10111011, 0b01100010, 0b00101010, 0b00111001, 0b00101100, 0b00100100, 0b10111010, 0b00100000, 0b00101000};
 
-int Td = 0b11101000;
-int Tb = 0b00100000;
-
-int Bd = 0b00100000;
-int Bb = 0b00000000;
-
+//f-0, a-1, b-2, e-3, d-4, dot-5,c-6, g-7
 int main(void)
 {
     DDRB = 0xFF;
@@ -27,16 +22,32 @@ int main(void)
     while (1) 
     { 
 	
-	PORTB = 4;
-	PORTD = A;
+	PORTD = seq[0];
+	PORTB = 1; 
 	_delay_ms(delay_time);
-	
-	PORTB = 1+Tb;
-	PORTD = Td;
-	_delay_ms(delay_time);
-	
+	PORTD = seq[1];
 	PORTB = 2;
-	PORTD = Bd;
+	_delay_ms(delay_time);
+	PORTD = seq[2];
+	PORTB = 4;
+	_delay_ms(delay_time);
+	PORTD = seq[3];
+	PORTB = 1;
+	_delay_ms(delay_time);
+	PORTD = seq[4];
+	PORTB = 2;
+	_delay_ms(delay_time);
+	PORTD = seq[5];
+	PORTB = 4;
+	_delay_ms(delay_time);
+	PORTD = seq[6];
+	PORTB = 1;
+	_delay_ms(delay_time);
+	PORTD = seq[7];
+	PORTB = 2;
+	_delay_ms(delay_time);
+	PORTD = seq[8];
+	PORTB = 4;
 	_delay_ms(delay_time);
 	
     }
