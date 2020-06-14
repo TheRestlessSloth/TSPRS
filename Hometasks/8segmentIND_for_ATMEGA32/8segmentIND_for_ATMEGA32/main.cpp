f9dbf7f75be1e09b143b1b392876c2b995b3bdb0
TheRestlessSloth <TheRestlessSloth@users.noreply.github.com>
2020-06-13 20:26:49 -0700

    Забыл загрузить

--- Hometasks/8segmentIND_for_ATMEGA32/8segmentIND_for_ATMEGA32/main.cpp
+++ Hometasks/8segmentIND_for_ATMEGA32/8segmentIND_for_ATMEGA32/main.cpp
@@ -1,10 +1,3 @@
-/*
- * 8segment indicator.cpp
- *
- * Created: 08.04.2020 12:31:33
- * Author : timer
- */ 
-
 #include <avr/io.h>
 #define F_CPU 16000000UL
 #include <util/delay.h>
@@ -16,25 +9,24 @@ int seq[10] = {0b10100000, 0b10111011, 0b01100010, 0b00101010, 0b00111001, 0b001
 //f-0, a-1, b-2, e-3, d-4, dot-5,c-6, g-7
 int main(void)
 {
-    DDRB = 0xFF;
+	DDRB = 0xFF;
 	DDRD = 0xFF;
 	/* Replace with your application code */
 	PORTB = 1;
-    
-	while (1) 
-    { 
+	
+	while (1)
+	{
 
 		for(int i = 0; i<9; i++)
 		{
-			if(PORTB > 4)
+			if(PORTB = 128)
 				PORTB = 1;
 			
 			PORTD = seq[i];
 			PORTB = PORTB << 1;
-		
-			//_delay_ms(delay_time);
+			
+			_delay_ms(delay_time);
 
 		}
 	}
 }
-
